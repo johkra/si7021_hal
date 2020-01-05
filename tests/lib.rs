4a10448e79bde8c4b670a76c38cd76874bb9f0f7
Johannes Krampf <johannes.krampf@googlemail.com>
2020-01-05 14:09:00 +0100

    Move tests to embedded-hal-mock

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,72 +1,19 @@
-extern crate embedded_hal;
-extern crate si7021_hal;
-
-use embedded_hal::blocking::i2c::Write;
-use embedded_hal::blocking::i2c::WriteRead;
-use std::collections::HashMap;
-
-#[derive(Debug, PartialEq)]
-enum Error {
-    NoResponse,
-    UnknownCommand,
-}
-
-// TODO: Move to embedded-hal-mock crate?
-#[derive(Debug)]
-struct MockI2c<'a> {
-    request_response_map: HashMap<&'a [u8], &'a [u8]>,
-}
-
-impl<'a> WriteRead for MockI2c<'a> {
-    type Error = Error;
-    fn write_read(
-        &mut self,
-        _address: u8,
-        bytes: &[u8],
-        buffer: &mut [u8],
-    ) -> Result<(), Self::Error> {
-        match self.request_response_map.get(bytes) {
-            Some(buf) => buffer.copy_from_slice(buf),
-            None => return Err(Error::NoResponse),
-        }
-        Ok(())
-    }
-}
-
-impl<'a> Write for MockI2c<'a> {
-    type Error = Error;
-    fn write(&mut self, _address: u8, bytes: &[u8]) -> Result<(), Self::Error> {
-        if !self.request_response_map.contains_key(bytes) {
-            return Err(Error::UnknownCommand);
-        }
-        Ok(())
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use super::Error;
-    use super::MockI2c;
+    use embedded_hal_mock::i2c::{Mock as I2cMock, Transaction as I2cTransaction};
+    use embedded_hal_mock::MockError;
     use si7021_hal::MeasurementResolution;
     use si7021_hal::Si7021;
-    use std::collections::HashMap;
-
-    // Type coercion from &[{integer}; N] to &[u8] failed when using the maplit macros.
-    // Revisit usage of the crate after transitive coercions
-    // (https://github.com/rust-lang/rust/issues/18602) are implemented?
-    fn make_map<'a>(kvpairs: &[(&'a [u8], &'a [u8])]) -> HashMap<&'a [u8], &'a [u8]> {
-        let mut map: HashMap<&[u8], &[u8]> = HashMap::new();
-        for (k, v) in kvpairs {
-            map.insert(k, v);
-        }
-        map
-    }
+    use std::io::ErrorKind;
 
     #[test]
     fn get_temperature() {
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[(&[0xe3], &[0x66, 0x4c, 0x4f])]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[I2cTransaction::write_read(
+            0x40,
+            vec![0xe3],
+            vec![0x66, 0x4c, 0x4f],
+        )]));
+
         let temperature = si7021.temperature();
         assert!(temperature.is_ok());
         assert_eq!(temperature.unwrap(), 2336);
@@ -74,9 +21,12 @@ mod tests {
 
     #[test]
     fn get_temperature_crc_failure() {
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[(&[0xe3], &[0x66, 0x4c, 0xff])]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[I2cTransaction::write_read(
+            0x40,
+            vec![0xe3],
+            vec![0x66, 0x4c, 0xff],
+        )]));
+
         let temperature = si7021.temperature();
         assert!(temperature.is_err());
         assert_eq!(temperature.unwrap_err(), si7021_hal::Error::ChecksumFailure);
@@ -84,12 +34,11 @@ mod tests {
 
     #[test]
     fn get_humdity_and_temperature() {
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[
-                (&[0xe5], &[0xa1, 0xa6, 0x51]),
-                (&[0xe0], &[0x66, 0x44]),
-            ]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[
+            I2cTransaction::write_read(0x40, vec![0xe5], vec![0xa1, 0xa6, 0x51]),
+            I2cTransaction::write_read(0x40, vec![0xe0], vec![0x66, 0x44]),
+        ]));
+
         let humidity = si7021.humidity();
         assert!(humidity.is_ok());
         assert_eq!(humidity.unwrap(), 7292);
@@ -101,9 +50,12 @@ mod tests {
 
     #[test]
     fn get_humdity_crc_failure() {
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[(&[0xe5], &[0xa1, 0xa6, 0xff])]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[I2cTransaction::write_read(
+            0x40,
+            vec![0xe5],
+            vec![0xa1, 0xa6, 0xff],
+        )]));
+
         let humidity = si7021.humidity();
         assert!(humidity.is_err());
         assert_eq!(humidity.unwrap_err(), si7021_hal::Error::ChecksumFailure);
@@ -111,9 +63,12 @@ mod tests {
 
     #[test]
     fn get_humidity_temperature_without_humidity_measurement() {
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[(&[0xe0], &[0x00, 0x00])]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[I2cTransaction::write_read(
+            0x40,
+            vec![0xe0],
+            vec![0x00, 0x00],
+        )]));
+
         let temperature = si7021.temperature_rh_measurement();
         assert!(temperature.is_err());
         assert_eq!(
@@ -124,15 +79,19 @@ mod tests {
 
     #[test]
     fn get_serial_number() {
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[
-                (
-                    &[0xfa, 0x0f],
-                    &[0x84, 0xbe, 0x2c, 0x5b, 0xf9, 0x9e, 0xb1, 0xa8],
-                ),
-                (&[0xfc, 0xc9], &[0x15, 0xff, 0xb5, 0xff, 0xff, 0xcb]),
-            ]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[
+            I2cTransaction::write_read(
+                0x40,
+                vec![0xfa, 0x0f],
+                vec![0x84, 0xbe, 0x2c, 0x5b, 0xf9, 0x9e, 0xb1, 0xa8],
+            ),
+            I2cTransaction::write_read(
+                0x40,
+                vec![0xfc, 0xc9],
+                vec![0x15, 0xff, 0xb5, 0xff, 0xff, 0xcb],
+            ),
+        ]));
+
         let serial_number = si7021.serial_number();
         assert!(serial_number.is_ok());
         assert_eq!(serial_number.unwrap(), 0x842cf9b115ffffff);
@@ -140,15 +99,19 @@ mod tests {
 
     #[test]
     fn get_serial_number_crc_failure_id1() {
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[
-                (
-                    &[0xfa, 0x0f],
-                    &[0x84, 0xbe, 0x2c, 0x5b, 0xf9, 0x9e, 0xb1, 0xff],
-                ),
-                (&[0xfc, 0xc9], &[0x15, 0xff, 0xb5, 0xff, 0xff, 0xcb]),
-            ]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[
+            I2cTransaction::write_read(
+                0x40,
+                vec![0xfa, 0x0f],
+                vec![0x84, 0xbe, 0x2c, 0x5b, 0xf9, 0x9e, 0xb1, 0xff],
+            ),
+            I2cTransaction::write_read(
+                0x40,
+                vec![0xfc, 0xc9],
+                vec![0x15, 0xff, 0xb5, 0xff, 0xff, 0xcb],
+            ),
+        ]));
+
         let serial_number = si7021.serial_number();
         assert!(serial_number.is_err());
         assert_eq!(
@@ -159,15 +122,19 @@ mod tests {
 
     #[test]
     fn get_serial_number_crc_failure_id2() {
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[
-                (
-                    &[0xfa, 0x0f],
-                    &[0x84, 0xbe, 0x2c, 0x5b, 0xf9, 0x9e, 0xb1, 0xa8],
-                ),
-                (&[0xfc, 0xc9], &[0x15, 0xff, 0xb5, 0xff, 0xff, 0xff]),
-            ]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[
+            I2cTransaction::write_read(
+                0x40,
+                vec![0xfa, 0x0f],
+                vec![0x84, 0xbe, 0x2c, 0x5b, 0xf9, 0x9e, 0xb1, 0xa8],
+            ),
+            I2cTransaction::write_read(
+                0x40,
+                vec![0xfc, 0xc9],
+                vec![0x15, 0xff, 0xb5, 0xff, 0xff, 0xff],
+            ),
+        ]));
+
         let serial_number = si7021.serial_number();
         assert!(serial_number.is_err());
         assert_eq!(
@@ -178,9 +145,12 @@ mod tests {
 
     #[test]
     fn get_firmware_revision() {
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[(&[0x84, 0xb8], &[0x20])]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[I2cTransaction::write_read(
+            0x40,
+            vec![0x84, 0xb8],
+            vec![0x20],
+        )]));
+
         let firmware_revision = si7021.firmware_revision();
         assert!(firmware_revision.is_ok());
         assert_eq!(firmware_revision.unwrap(), 0x20);
@@ -188,32 +158,37 @@ mod tests {
 
     #[test]
     fn get_firmware_revision_i2c_error() {
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[I2cTransaction::write_read(
+            0x40,
+            vec![0x84, 0xb8],
+            vec![0x20],
+        )
+        .with_error(MockError::Io(ErrorKind::Other))]));
+
         let firmware_revision = si7021.firmware_revision();
         assert!(firmware_revision.is_err());
         assert_eq!(
             firmware_revision.unwrap_err(),
-            si7021_hal::Error::I2c(Error::NoResponse)
+            si7021_hal::Error::I2c(MockError::Io(ErrorKind::Other))
         );
     }
 
     #[test]
     fn reset() {
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[(&[0xfe], &[])]),
-        });
-        let firmware_revision = si7021.reset();
-        assert!(firmware_revision.is_ok());
+        let mut si7021 = Si7021::new(I2cMock::new(&[I2cTransaction::write(0x40, vec![0xfe])]));
+
+        let reset = si7021.reset();
+        assert!(reset.is_ok());
     }
 
     #[test]
     fn set_measurement_resolution() {
         // Fill reserved bits with 1 and ensure they're written back
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[(&[0xe7], &[0xff]), (&[0xe6, 0x7e], &[])]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[
+            I2cTransaction::write_read(0x40, vec![0xe7], vec![0xff]),
+            I2cTransaction::write(0x40, vec![0xe6, 0x7e]),
+        ]));
+
         let measurement_resolution =
             si7021.set_measurement_resolution(MeasurementResolution::Rh12Temp14);
         assert!(measurement_resolution.is_ok());
@@ -221,9 +196,12 @@ mod tests {
 
     #[test]
     fn get_measurement_resolution() {
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[(&[0xe7], &[0x01])]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[I2cTransaction::write_read(
+            0x40,
+            vec![0xe7],
+            vec![0x01],
+        )]));
+
         let measurement_resolution = si7021.measurement_resolution();
         assert!(measurement_resolution.is_ok());
         assert_eq!(
@@ -234,9 +212,12 @@ mod tests {
 
     #[test]
     fn get_heater_off() {
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[(&[0xe7], &[0x00]), (&[0x11], &[0x00])]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[I2cTransaction::write_read(
+            0x40,
+            vec![0xe7],
+            vec![0x11],
+        )]));
+
         let heater = si7021.heater();
         assert!(heater.is_ok());
         assert_eq!(heater.unwrap(), None);
@@ -244,9 +225,11 @@ mod tests {
 
     #[test]
     fn get_heater_on() {
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[(&[0xe7], &[0x04]), (&[0x11], &[0x0a])]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[
+            I2cTransaction::write_read(0x40, vec![0xe7], vec![0x04]),
+            I2cTransaction::write_read(0x40, vec![0x11], vec![0x0a]),
+        ]));
+
         let heater = si7021.heater();
         assert!(heater.is_ok());
         assert_eq!(heater.unwrap(), Some(0x0a));
@@ -255,14 +238,13 @@ mod tests {
     #[test]
     fn set_heater_off() {
         // Fill reserved bits with 1 and ensure they're written back
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[
-                (&[0xe7], &[0xff]),
-                (&[0x11], &[0xff]),
-                (&[0xe6, 0xfb], &[]),
-                (&[0x51, 0xff], &[]),
-            ]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[
+            I2cTransaction::write_read(0x40, vec![0xe7], vec![0xff]),
+            I2cTransaction::write_read(0x40, vec![0x11], vec![0xff]),
+            I2cTransaction::write(0x40, vec![0xe6, 0xfb]),
+            I2cTransaction::write(0x40, vec![0x51, 0xff]),
+        ]));
+
         let heater = si7021.set_heater(None);
         assert!(heater.is_ok());
     }
@@ -270,14 +252,13 @@ mod tests {
     #[test]
     fn set_heater_on() {
         // Fill reserved bits with 1 and ensure they're written back
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[
-                (&[0xe7], &[0xfb]),
-                (&[0x11], &[0xf0]),
-                (&[0xe6, 0xff], &[]),
-                (&[0x51, 0xfa], &[]),
-            ]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[
+            I2cTransaction::write_read(0x40, vec![0xe7], vec![0xfb]),
+            I2cTransaction::write_read(0x40, vec![0x11], vec![0xf0]),
+            I2cTransaction::write(0x40, vec![0xe6, 0xff]),
+            I2cTransaction::write(0x40, vec![0x51, 0xfa]),
+        ]));
+
         let heater = si7021.set_heater(Some(0x0a));
         assert!(heater.is_ok());
     }
@@ -285,9 +266,11 @@ mod tests {
     #[test]
     fn set_heater_invalid_power() {
         // Fill reserved bits with 1 and ensure they're written back
-        let mut si7021 = Si7021::new(MockI2c {
-            request_response_map: make_map(&[(&[0xe7], &[0xfb]), (&[0x11], &[0xf0])]),
-        });
+        let mut si7021 = Si7021::new(I2cMock::new(&[
+            I2cTransaction::write_read(0x40, vec![0xe7], vec![0xfb]),
+            I2cTransaction::write_read(0x40, vec![0x11], vec![0xf0]),
+        ]));
+
         let heater = si7021.set_heater(Some(0xf0));
         assert!(heater.is_err());
         assert_eq!(heater.unwrap_err(), si7021_hal::Error::InvalidHeaterLevel);
